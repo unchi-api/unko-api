40fcbbe4ec2248a9635ac87ffbd3143e9e1bb910
Kousuke <yoshida.kousuke.is2026@gmail.com>
2024-11-22 02:17:04 +0900

    printの改変

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from app import create_app
 
 app = create_app()
 
+print("import_name:", app.import_name)
+print("static_folder:", app.static_folder)
+print("template_folder:", app.template_folder)
+
 if __name__ == "__main__":
     app.run(debug=True)
